2331b11723b5ebed3dafaa1cee7497a509158bad
Carlos Signor <caiosignor@gmail.com>
2026-08-29 15:23:52 -0300

    add operation to get all available seats for movie and theater

---
+++ code/include/MovieSessionDatabase/Operations/Get/GetAllAvailableSeatsForMovieAndTheater.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <span>
+#include <utility>
+
+#include "MovieSessionVisitor.hpp"
+
+class GetAllAvailableSeatsForMovieAndTheater : public MovieSessionVisitor
+{
+public:
+    explicit GetAllAvailableSeatsForMovieAndTheater(std::string_view theater,std::string_view movie, std::span<std::string> out);
+    ~GetAllAvailableSeatsForMovieAndTheater() override = default;
+
+private:
+    std::span<std::string> m_outData;
+    std::string_view m_movieName;
+    std::string_view m_theaterName;
+    size_t m_dataIterator{0};
+    void visit(MovieScreeningPtrType movie) override;
+};
---
+++ code/src/MovieSessionDatabase/Operations/Get/GetAllAvailableSeatsForMovieAndTheater.cpp
@@ -0,0 +1,38 @@
+#include "GetAllAvailableSeatsForMovieAndTheater.hpp"
+
+GetAllAvailableSeatsForMovieAndTheater::GetAllAvailableSeatsForMovieAndTheater(std::string_view theater,std::string_view movie, std::span<std::string> out)
+    : m_outData(out), m_movieName(movie), m_theaterName(theater)
+{
+    m_isFinished = false;
+    m_retCode = DatabaseError::NotFound;
+}
+
+void GetAllAvailableSeatsForMovieAndTheater::visit(MovieScreeningPtrType movie)
+{
+    if (m_dataIterator > m_outData.size())
+    {
+        finish();
+        m_retCode = DatabaseError::OutOfMemory;
+        return;
+    }
+
+    if(movie->movie_name == m_movieName && movie->theater_name == m_theaterName)
+    {
+        for(const auto& [seat, isfree] : movie->m_availableSeats)
+        {
+            if(isfree)
+            {
+                if (m_dataIterator >= m_outData.size())
+                {
+                    finish();
+                    m_retCode = DatabaseError::OutOfMemory;
+                    return;
+                }
+
+                m_outData[m_dataIterator++] = seat;
+            }
+        }
+        finish();
+        m_retCode = DatabaseError::OK;
+    }
+}
