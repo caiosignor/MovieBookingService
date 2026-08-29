af5e7fef90ac674623a9a9d2f501eaa61be3d678
Carlos Signor <caiosignor@gmail.com>
2026-08-29 18:31:29 -0300

    expected to fail ttest to test pipeline

--- tests/DatabaseTest.cpp
+++ tests/DatabaseTest.cpp
@@ -123,7 +123,7 @@ void db_GetAllTheaterShowingTheMovie()
         return !s.empty();
     });
         
-    expect(numberOfTheater == 2,
+    expect(numberOfTheater == 3,
         std::format("there are currently 2 theaters showing {}, number of theaters {}", TestData::MovieName, numberOfTheater));
 
 }
